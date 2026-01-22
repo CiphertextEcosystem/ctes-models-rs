a6990d6f40c6bc937af52c11144bbd7ced06fc3a
rebase <rebased.again@gmail.com>
2026-01-21 18:20:59 -0600

    fix export

--- ctes_models/src/lib.rs
+++ ctes_models/src/lib.rs
@@ -2,3 +2,6 @@
 pub mod models {
     include!(concat!(env!("OUT_DIR"), "/model.ciphertext.rs"));
 }
+
+// Re-export types for convenient access
+pub use models::{Ciphertext, CiphertextMetadata, EncodingMetadata, Encoding};
